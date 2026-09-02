64f937c940df9a741aae8d0219a2f65a80320971
devopsFinartis <infrastructure@finartis.com>
2026-09-03 00:52:16 +0800

    added test cases

--- tests/PEW/positions.spec.js
+++ tests/PEW/positions.spec.js
@@ -2,16 +2,12 @@
 
 test('test @FP-TC-343', async ({ page }) => {
   await page.goto('http://192.168.137.2:7080/realms/prospero/protocol/openid-connect/auth?response_type=code&client_id=rest-api-client&scope=openid%20profile%20email&state=TTiUdn1xfMIAyf46wOW-QF1TbFgf9SnSlFcbzMoRqa8%3D&redirect_uri=http://192.168.137.2:8800/login/oauth2/code/keycloak&nonce=mshjhR3NpgFxfibIqby-Y9epBy-RJZlLM3X573F2ve4&code_challenge=rl6MoilaueXoDFKX2Rg22SzPg_nXhqGY5IgAfS9sRxI&code_challenge_method=S256');
-
+  await page.getByRole('textbox', { name: 'Username or email' }).click();
   await page.getByRole('textbox', { name: 'Username or email' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Username or email' }).press('Tab');
   await page.getByRole('textbox', { name: 'Password' }).fill('admin');
   await page.getByRole('button', { name: 'Sign In' }).click();
-
   await page.getByRole('link', { name: 'Positions' }).click();
-  await expect(page).toHaveURL(/\/1\/position\/blotter/);
-
-  const searchButton = page.getByRole('button', { name: 'Search' });
-  await expect(searchButton).toBeVisible({ timeout: 15000 });
-  await searchButton.click();
+  await page.getByRole('button', { name: 'Search' }).click();
 });
 
